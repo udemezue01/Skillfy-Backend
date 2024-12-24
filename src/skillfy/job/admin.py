6913392320017ac815e9a3f641c4f5f16567a9ce
Udemezue John <johnzeus14@gmail.com>
2024-12-24 07:15:08 +0100

    added Job model

---
+++ src/skillfy/job/admin.py
@@ -0,0 +1,20 @@
+
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+
+class Job(models.Model):
+
+    user               		 =   models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title               	 =   models.CharField(max_length=3000, blank =True)
+    description               =   models.FileField(blank = True)
+    created_at        		 =   models.DateTimeField(auto_now =False , auto_now_add=True)
+
+
+    def __str__(self):
+
+        return f'self.user.full_name'
+
+
