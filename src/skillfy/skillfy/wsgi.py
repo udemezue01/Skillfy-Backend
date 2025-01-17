cdc3e33adf58350269c995dc7b9f54433d6957e7
Udemezue John <johnzeus14@gmail.com>
2025-01-17 04:40:42 +0100

    Update wsgi.py

--- src/skillfy/skillfy/wsgi.py
+++ src/skillfy/skillfy/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skillfy.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skillfy.settings.prod')
 
 application = get_wsgi_application()
